8898a4965e214de0edf7f61d51a50f24ec9bea83
Amina Jusic <amina_jusic@yahoo.com>
2019-02-06 14:43:26 +0000

    Step 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,9 @@
 require 'docking_station'
 describe DockingStation do
+  it 'respond to release bike' do
+    new_DockingStation = DockingStation.new
+    expect(new_DockingStation).to respond_to(:release_bike)
+  end
+
+  it { is_expected.to respond_to :release_bike }
 end
