9dcf4909c71d93660c6a82aaf31b8358d9c54b6f
Amina Jusic <amina_jusic@yahoo.com>
2019-02-06 16:16:19 +0000

    Step 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,4 +6,26 @@ describe DockingStation do
   end
 
   it { is_expected.to respond_to :release_bike }
+
+  # test if gets a bike
+  it 'gets a bike' do
+    # arrange
+    docking_station = DockingStation.new
+    # act
+    bike = docking_station.release_bike
+    # assertion
+    expect(bike.class).to eq Bike
+  end
+
+  # test if bike working
+  it 'sees if bike is working' do
+    # arrange
+    docking_station = DockingStation.new
+    # act
+    bike = docking_station.release_bike
+    # assertion
+    expect(bike.working?).to eq true
+  end
+
+
 end
